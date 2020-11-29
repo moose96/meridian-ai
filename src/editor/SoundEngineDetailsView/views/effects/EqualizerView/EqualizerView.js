2145a73cf649d1d79db7222a24e269db696bd17c
Piotr Łosiak <piotr.losiak96@gmail.com>
2020-11-29 21:45:22 +0100

    add equalizer view

--- src/editor/SoundEngineDetailsView/views/effects/EqualizerView/EqualizerView.js
+++ src/editor/SoundEngineDetailsView/views/effects/EqualizerView/EqualizerView.js
@@ -3,9 +3,11 @@ import React from 'react';
 import Filter from './Filter';
 
 function EqualizerView({ data }) {
+  const filters = data?.filters;
+
   return (
     <div className="equalizer-view">
-
+      {filters.map(filter => <Filter {...filter} />)}
     </div>
   );
 }

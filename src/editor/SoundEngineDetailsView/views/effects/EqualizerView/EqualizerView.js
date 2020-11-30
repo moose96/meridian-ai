227cc707c5221aef073ca26dcf78648c76cc4aff
Piotr Łosiak <piotr.losiak96@gmail.com>
2020-11-30 01:06:55 +0100

    add chart

--- src/editor/SoundEngineDetailsView/views/effects/EqualizerView/EqualizerView.js
+++ src/editor/SoundEngineDetailsView/views/effects/EqualizerView/EqualizerView.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries } from 'react-vis';
 
 import DetailsGroup from '../../../../DetailsGroup';
 import Filter from './Filter';
+import 'react-vis/dist/style.css';
 
 function EqualizerView({ data }) {
   const filters = data?.filters;
-  console.log(filters);
+  let chartData = [];
+
+  if (data) {
+    for (let i = 0; i < data.frequencies.length; i++) {
+      chartData.push({
+        x: data?.frequencies[i],
+        y: data?.frequencyResponse[i]
+      });
+    }
+  }
 
   return (
     <DetailsGroup title="Equalizer">
+      <XYPlot width={800} height={200} yDomain={[0.5, 1.5]}>
+        {/* <VerticalGridLines /> */}
+        <HorizontalGridLines />
+        <LineSeries color="blue" data={chartData}/>
+        <XAxis tickValues={[62.5, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]}/>
+        <YAxis tickValues={[0.5, 1.0, 1.5]} />
+      </XYPlot>
       {filters.map((filter, index) => <Filter key={`eq-filter-${index}`} {...filter} />)}
     </DetailsGroup>
   );
